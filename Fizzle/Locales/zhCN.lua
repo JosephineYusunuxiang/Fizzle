ee812ea4363c18047c531103abafc83bc32c4b70
JosephineYusunuxiang <128058341+JosephineYusunuxiang@users.noreply.github.com>
2023-03-20 14:38:38 +0800

    Add files via upload

---
+++ Fizzle/Locales/zhCN.lua
@@ -0,0 +1,27 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("Fizzle", "zhCN")
+if not L then return end
+
+-- Core.lua
+L["Displays Item Quality and Durability in the character frame."] = "在装备栏中显示品质与耐久度。"
+L["Percent"] = "百分比/数值"
+L["Toggle percentage display."] = "切换百分比/数值显示耐久度的形式。"
+L["Border"] = "边框"
+L["Toggle quality borders."] = "按品质给人物面板物品边框着色。"
+L["Invert"] = "磨损率"
+L["Show numbers the other way around. Eg. 0% = full durability , 100 = no durability."] = "另一种显示方式。 例如: 0% = 完好的；100% = 完全损坏。"
+L["Hide Text"] = "隐藏文字"
+L["Hide durability text."] = "始终隐藏不显示百分比或者数值耐久度文字。"
+L["Show Full"] = "显示完好耐久度"
+L["Show durability when full."] = "当物品完好时也显示耐久度。"
+L["Show iLevel"] = "显示装备等级"
+L["Show items' iLevel"] = "显示你人物面板上的装备等级。"
+L["Inspect"] = "观察别人装备染色"
+L["Show item quality when inspecting people."] = "观察别人时显示他们人物面板的物品品质。"
+L["Inspect iLevels"] = "观察别人装备等级"
+L["Show the iLevel on an inspected characters items."] = "观察别人时显示他们人物面板的装备等级。"
+
+L["Show Repair Cost"] = "显示修理费" --没见过以下四条，他们为什么会出现？
+L["Show the total repair cost of your items."] = "显示所有装备修理所需的费用。"
+L["Discount"] = "折扣"
+L["Show the repair cost minus the 10% faction discount."] = "显示受声望减免后的修理费(-10%)。"
+
